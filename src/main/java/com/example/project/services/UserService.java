44bd6dac2d2921e75ebf3577073805fcfc37c1fe
Ulukbek <tu12257@auca.kg>
2024-05-02 16:55:42 +0600

    Fields filtration are done

--- src/main/java/com/example/project/services/UserService.java
+++ src/main/java/com/example/project/services/UserService.java
@@ -16,8 +16,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.regex.Pattern;
-
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
@@ -30,10 +28,40 @@ public class UserService {
     AuthenticationManager authenticationManager;
 
     JwtUtils jwtUtils;
-    static Pattern EMAIL_PATTERN =
-            Pattern.compile("^[A-Za-z]+@[A-Za-z]+\\.[A-Za-z]+$");
 
-    static Pattern VALID_CHARACTERS_PATTERN = Pattern.compile("^[a-zA-Z]+$");
+    public ResponseEntity<String> register(RegisterDto registerDto) {
+        if (userRepository.findByUsername(registerDto.getUsername()) != null ||
+                userRepository.findByEmail(registerDto.getEmail()) != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("User with this username or email already exists");
+        }
+        User user = new User();
+
+        String email = registerDto.getEmail();
+        if(!isValidEmail(email)){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email does not match to requirements");
+        }
+
+        String username = registerDto.getUsername();
+        if(!isValidLogin(username)){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username does not match to requirements");
+        }
+
+        String password = registerDto.getPassword();
+        String repeatedPassword = registerDto.getRepeatedPassword();
+        if(!isValidPassword(password)){
+            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Password does not match to requirements");
+        }
+        if(!password.equals(repeatedPassword)){
+            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Passwords do not match to each other");
+        }
+        //если все валидации пройдены
+        user.setEmail(email);
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+
+        userRepository.save(user);
+        return ResponseEntity.ok().body("User registered successfully");
+    }
 
     public ResponseEntity<String> authenticate(LoginDto loginDto) {
         try {
@@ -53,62 +81,77 @@ public class UserService {
         }
     }
 
-    public ResponseEntity<String> register(RegisterDto registerDto) {
-        if (userRepository.findByUsername(registerDto.getUsername()) != null ||
-                userRepository.findByEmail(registerDto.getEmail()) != null) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("User with this username or email already exists");
-        }
-        User user = new User();
-
-        String email = registerDto.getEmail();
-        if(!EMAIL_PATTERN.matcher(email).matches()){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email does not match to requirements");
-        }
-
-        String username = registerDto.getUsername();
-        if(!VALID_CHARACTERS_PATTERN.matcher(username).matches()){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username does not match to requirements");
-        }
-
-        String password = registerDto.getPassword();
-        String repeatedPassword = registerDto.getRepeatedPassword();
-        if(!isValidPassword(password)){
-            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Password does not match to requirements");
-        }
-        if (!isValidPassword(repeatedPassword)) {
-            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Repeated password does not match to requirements");
-        }
-        if(!password.equals(repeatedPassword)){
-            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Passwords do not match to each other");
-        }
-        //если все валидации пройдены
-        user.setEmail(email);
-        user.setUsername(username);
-        user.setPassword(passwordEncoder.encode(password));
-
-        userRepository.save(user);
-        return ResponseEntity.ok().body("User registered successfully");
-    }
     public ResponseEntity<String> home(){
         return ResponseEntity.ok().body("Home....");
     }
-    public boolean isValidPassword(String password){
-        if (password == null) {
+    public boolean isValidEmail(String email) {
+        // Проверяем, что строка не пуста и содержит символ '@'
+        if (email == null || !email.contains("@")) {
+            System.out.println("Поле почты является пустым или не содержит @");
             return false;
         }
-        if(password.length() < 8 || password.length() > 15){
+
+        // Разделяем строку по символу '@' на две части
+        String[] parts = email.split("@");
+        if (parts.length != 2) {
+            return false; // Если в адресе нет или более одного символа '@', он недопустим
+        }
+
+        // Проверяем, что обе части не пусты
+        String username = parts[0];
+        String domain = parts[1];
+        if (username.isEmpty() || domain.isEmpty()) {
+            System.out.println("Одна из частей или обе части являются пустыми");
             return false;
         }
-        if(!password.matches(".*[a-z].*") || !password.matches(".*[A-Z].*")){
+
+        // Проверяем, что в домене есть хотя бы одна точка
+        if (!domain.contains(".")) {
+            System.out.println("Домен не содержит одну точку ");
             return false;
         }
-        if(!password.matches(".*\\d.*")){
-            return false;
-        }
-        if(!password.matches(".*[!@#%^&*].*")){
+
+        // Проверяем, что в имени пользователя и домене нет недопустимых символов
+        if (!username.matches("[a-zA-Z0-9]+") || !domain.matches("[a-zA-Z0-9.]+")) {
+            System.out.println("В имени пользователя или в домене есть недопустимые символы");
             return false;
         }
         return true;
     }
+
+
+    public boolean isValidLogin(String login){
+        // Проверяем, что login содержит только прописные и строчные английские буквы
+        if (!login.matches("[a-zA-Z]+")) {
+            System.out.println("Отсутствуют буквы английского алфавита верхнего или нижнего регистра");
+            return false;
+        }
+        return true;
+    }
+    public boolean isValidPassword(String password){
+        if (password == null) {
+            System.out.println("Пароль равен null");
+            return false;
+        }
+        if(password.length() < 8 || password.length() > 15){
+            System.out.println("Длина пароля не в допустимом диапазоне");
+            return false;
+        }
+        // Проверяем, что login содержит только прописные и строчные английские буквы
+        if (!password.matches("[a-zA-Z]+")) {
+            System.out.println("Отсутствуют английские буквы верхнего или нижнего регистра");
+            return false;
+        }
+        if(!password.matches(".*\\d.*")){
+            System.out.println("Отсутствуют цифры");
+            return false;
+        }
+        if(!password.matches(".*[!@#%^&*].*")){
+            System.out.println("Отсутствуют специальные символы");
+            return false;
+        }
+        return true;
+    }
+
 }
 
